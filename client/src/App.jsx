2706ea150465c0268381a5c18c9605a8c0ce5c71
Harsh <harshb15003@gmail.com>
2024-03-11 22:51:22 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,34 @@
-import './App.css'
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Login from "./Pages/Login.jsx";
+import Signup from "./Pages/Signup";
+import SlotEntry from "./Pages/SlotEntry.jsx";
+import SlotExit from "./Pages/SlotExit.jsx";
+import NFTMint from "./Pages/nftMint.jsx";
+import ClaimNft from "./Pages/claimNft.jsx";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import Home from "./Pages/Home.jsx";
 
-function App() {
+
+
+const App = () => (
+  <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path ='/slotentry' element={<SlotEntry />} />
+        <Route path ='/slotexit' element={<SlotExit />} />
+        <Route path="/nftMint" element={<NFTMint/>} />
+        <Route path="/claimNft" element={<ClaimNft/>} />
+      </Routes>
+      <ToastContainer/>
+    </Router>
   
 
-  return (
-    <>
-     <div>Hello</div>
-    </>
-  )
-}
+  </div>
+);
 
-export default App
+export default App;
